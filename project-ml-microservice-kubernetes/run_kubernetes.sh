8cc8bfc286d2c154beddc036d8fc16e2c3832abe
Pedram Tadayoni <tadayoni@gmail.com>
2019-08-02 00:25:28 -0700

    feat: added run_kubernetes.sh and its output

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=pedramt/udacityproject:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacityproject --image=docker.io/$dockerpath --port=8080 --labels="app=udacityproject" --image-pull-policy=IfNotPresent
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+container=$(kubectl get pod | grep -P "udacityproject-[^\s]*\s" -o)
+kubectl port-forward $container 8000:80
