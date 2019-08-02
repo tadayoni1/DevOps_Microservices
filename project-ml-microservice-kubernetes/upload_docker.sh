d928f03db24e55345d1b03333a01599056a3243a
Pedram Tadayoni <tadayoni@gmail.com>
2019-08-01 23:32:46 -0700

    feat: added upload_docker.sh

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=pedramt/udacityproject
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+password=$(cat ~/dockerpassword)
+docker login -u pedramt -p $password
+docker tag udacityproject $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
